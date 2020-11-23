7b937b9d0361decbaf2b87015e773e2ae07451b9
niyzora <niyzora@gmail.com>
2020-11-24 01:39:07 +0600

    Map changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+from os import path
 from settings import *
 from sprites import *
 
@@ -14,14 +15,21 @@ class Game:
         self.load_data()
 
     def load_data(self):
-        pass
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        with open(path.join(game_folder, 'map.txt') , 'rt') as f:
+            for line in f:
+                self.map_data.append(line)
 
     def new(self):
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
-        self.player = Player(self, 10, 10)
-        for x in range(10, 20):
-            Wall(self, x, 5)
+        for row, tiles in enumerate(self.map_data):
+            for col, tile in enumerate(tiles):
+                if tile =='1':
+                    Wall(self, col, row)
+                if tile =='P':
+                    self.player = Player(self, col, row)
 
     def run(self):
         self.playing = True
