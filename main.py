003eebbfdd182745fb622375b82a9ebf28cf2187
zhangirtastemir <zhangirkhantastemir@gmail.com>
2020-11-28 00:47:44 +0600

    check2
    Signed-off-by: zhangirtastemir <zhangirkhantastemir@gmail.com>

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ from settings import *
 from sprites import *
 from tilemap import *
 
-
 class Game:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
-        pg.key.set_repeat(500, 100)
         self.load_data()
 
     def load_data(self):
@@ -22,21 +20,22 @@ class Game:
         self.player_img = pg.image.load(path.join(img_folder, PLAYER_IMG)).convert_alpha()
 
     def new(self):
+        # initialize all variables and do all the setup for a new game
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
         for row, tiles in enumerate(self.map.data):
             for col, tile in enumerate(tiles):
-                if tile =='1':
+                if tile == '1':
                     Wall(self, col, row)
-                if tile =='P':
+                if tile == 'P':
                     self.player = Player(self, col, row)
         self.camera = Camera(self.map.width, self.map.height)
 
-
     def run(self):
+        # game loop - set self.playing = False to end the game
         self.playing = True
         while self.playing:
-            self.dt = self.clock.tick(FPS) / 1000
+            self.dt = self.clock.tick(FPS) / 1000.0
             self.events()
             self.update()
             self.draw()
@@ -46,27 +45,26 @@ class Game:
         sys.exit()
 
     def update(self):
+        # update portion of the game loop
         self.all_sprites.update()
         self.camera.update(self.player)
-
-
-    def draw(self):
-        self.screen.fill(BGCOLOR)
-        self.draw_grid()
-        #self.all_sprites.draw(self.screen)
-        for sprite in self.all_sprites:
-            self.screen.blit(sprite.image, self.camera.apply(sprite))
-        pg.display.flip()
-
-
 
     def draw_grid(self):
         for x in range(0, WIDTH, TILESIZE):
             pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
-        for y in range(0, WIDTH, TILESIZE):
+        for y in range(0, HEIGHT, TILESIZE):
             pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
 
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
+        # pg.draw.rect(self.screen, WHITE, self.player.hit_rect, 2)
+        pg.display.flip()
+
     def events(self):
+        # catch all events here
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.quit()
@@ -74,14 +72,13 @@ class Game:
                 if event.key == pg.K_ESCAPE:
                     self.quit()
 
-
     def show_start_screen(self):
         pass
 
     def show_go_screen(self):
         pass
 
-
+# create the game object
 g = Game()
 g.show_start_screen()
 while True:
--- tilemap.py
+++ tilemap.py
@@ -1,12 +1,16 @@
 import pygame as pg
 from settings import *
 
+def collide_hit_rect(one, two):
+    return one.hit_rect.colliderect(two.rect)
+
 class Map:
     def __init__(self, filename):
         self.data = []
         with open(filename, 'rt') as f:
             for line in f:
                 self.data.append(line.strip())
+
         self.tilewidth = len(self.data[0])
         self.tileheight = len(self.data)
         self.width = self.tilewidth * TILESIZE
@@ -22,15 +26,12 @@ class Camera:
         return entity.rect.move(self.camera.topleft)
 
     def update(self, target):
-        #camera shift calc
-        x = -target.rect.x + int(WIDTH/2)
-        y = -target.rect.y + int(HEIGHT/2)
-        #limit to scroll
-        x = min(0, x)
-        y = min(0, y)
-        x = max(-(self.width - WIDTH), x)  #right
-        y = max(-(self.height - HEIGHT), y) #bottom
+        x = -target.rect.centerx + int(WIDTH / 2)
+        y = -target.rect.centery + int(HEIGHT / 2)
+
+        # limit scrolling to map size
+        x = min(0, x)  # left
+        y = min(0, y)  # top
+        x = max(-(self.width - WIDTH), x)  # right
+        y = max(-(self.height - HEIGHT), y)  # bottom
         self.camera = pg.Rect(x, y, self.width, self.height)
-
-
-
